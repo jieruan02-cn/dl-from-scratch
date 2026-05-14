c0cbf944a7bd6b8804ddbe26058267dd569fbec4
jieruan <jieruan02@gmail.com>
2026-05-14 11:53:42 -0500

    Implement Hardshrink.

--- core_modules/activation.py
+++ core_modules/activation.py
@@ -838,3 +838,33 @@ def tanhshrink(input):
 class Tanhshrink(nn.Module):
     def forward(self, input):
         return tanhshrink(input)
+
+
+class HardshrinkFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(input, lambd):
+        return torch.where((input < -lambd) | (input > lambd), input, 0.0)
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        if inputs[0].requires_grad:
+            ctx.save_for_backward(output)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (out,) = ctx.saved_tensors
+        grad_input = grad_output * (out != 0.0)
+        return grad_input, None
+
+
+def hardshrink(input, lambd=0.5):
+    return HardshrinkFunction.apply(input, lambd)
+
+
+class Hardshrink(nn.Module):
+    def __init__(self, lambd=0.5):
+        super().__init__()
+        self.lambd = lambd
+
+    def forward(self, input):
+        return hardshrink(input, self.lambd)
