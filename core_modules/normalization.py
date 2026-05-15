baa7b08f2f416f298778a7780df45a27fd593496
jieruan <jieruan02@gmail.com>
2026-05-15 14:42:43 -0500

    Add normalization.py.

---
+++ core_modules/normalization.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(
+        self,
+        normalized_shape,
+        eps=1e-05,
+        elementwise_affine=True,
+        bias=True,
+        device=None,
+        dtype=None,
+    ):
+        pass
+
+    def forward(self, input):
+        pass
