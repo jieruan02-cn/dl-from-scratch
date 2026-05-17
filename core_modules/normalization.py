1c9fc181dc4788ef5dff5d2a33d5372947d76bb0
jieruan <jieruan02@gmail.com>
2026-05-17 11:11:10 -0500

    Complete LN API.

--- core_modules/normalization.py
+++ core_modules/normalization.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 
 class LayerNormFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, weight, bias, agg_dim, normalized_shape, eps):
+    def forward(ctx, input, normalized_shape, weight, bias, eps):
+        agg_dim = tuple(range(-len(normalized_shape), 0))
         var, mean = torch.var_mean(input, dim=agg_dim, correction=0, keepdim=True)
         rstd = torch.rsqrt(var + eps)
         if weight is None:
@@ -37,12 +38,16 @@ class LayerNormFunction(torch.autograd.Function):
                 - torch.mean(grad_input * normed_input, dim=ctx.agg_dim, keepdim=True)
                 * normed_input
             ) * rstd
-        if ctx.needs_input_grad[1]:
-            grad_weight = (grad_output * normed_input).sum(ctx.batch_dim)
         if ctx.needs_input_grad[2]:
+            grad_weight = (grad_output * normed_input).sum(ctx.batch_dim)
+        if ctx.needs_input_grad[3]:
             grad_bias = grad_output.sum(ctx.batch_dim)
 
-        return grad_input, grad_weight, grad_bias, None, None, None
+        return grad_input, None, grad_weight, grad_bias, None
+
+
+def layer_norm(input, normalized_shape, weight=None, bias=None, eps=1e-05):
+    return LayerNormFunction.apply(input, normalized_shape, weight, bias, eps)
 
 
 class LayerNorm(nn.Module):
@@ -66,7 +71,7 @@ class LayerNorm(nn.Module):
             raise TypeError(
                 f"normalized_shape must be int, list, or torch.Size, got {type(normalized_shape)}"
             )
-        self.agg_dim = tuple(range(-len(self.normalized_shape), 0))
+
         self.eps = float(eps)
         self.weight = None
         self.bias = None
@@ -81,10 +86,5 @@ class LayerNorm(nn.Module):
 
     def forward(self, input):
         return LayerNormFunction.apply(
-            input,
-            self.weight,
-            self.bias,
-            self.agg_dim,
-            self.normalized_shape,
-            self.eps,
+            input, self.normalized_shape, self.weight, self.bias, self.eps
         )
