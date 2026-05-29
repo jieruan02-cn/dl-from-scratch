595db0d23a0b9e9f86c87103dab81c7fc3b85b83
jieruan <jieruan02@gmail.com>
2026-05-29 11:38:34 -0500

    Upgrade Dropout implementation.

--- core_modules/dropout.py
+++ core_modules/dropout.py
@@ -2,17 +2,39 @@ import torch
 import torch.nn as nn
 
 
-def dropout(input, p=0.5, training=True, inplace=False):
-    if training:
-        mask = torch.empty_like(input)
-        mask.bernoulli_(1 - p)
-        if inplace:
-            input.mul_(mask).div_(1 - p)
-            return input
+class DropoutFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, p, training, inplace):
+        mask = None
+        if training:
+            out = input if inplace else input.clone()
+            mask = torch.rand_like(input) > p
+            out.mul_(mask).div_(1 - p)
         else:
-            return input * mask / (1 - p)
-    else:
-        return input
+            out = input
+
+        if inplace:
+            ctx.mark_dirty(input)
+        if ctx.needs_input_grad[0]:
+            ctx.p = p
+            ctx.training = training
+            ctx.save_for_backward(mask)
+
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (mask,) = ctx.saved_tensors
+        if ctx.training:
+            grad_input = grad_output * mask
+            grad_input.div_(1 - ctx.p)
+        else:
+            grad_input = grad_output
+        return grad_input, None, None, None
+
+
+def dropout(input, p=0.5, training=True, inplace=False):
+    return DropoutFunction.apply(input, p, training, inplace)
 
 
 class Dropout(nn.Module):
