68a0c6fa632b7fe894a3ab0a5b0a6ec2114d5101
jieruan <jieruan02@gmail.com>
2026-06-03 15:22:02 -0500

    Implement mini SGD optimizer.

--- core_modules/optimizer.py
+++ core_modules/optimizer.py
@@ -28,3 +28,103 @@ class Optimizer:
 
     def step(self):
         raise NotImplementedError
+
+    def state_dict(self):
+        return self.state
+
+
+def sgd(
+    params,
+    d_p_list,
+    momentum_buffer_list,
+    has_sparse_grad=False,
+    foreach=None,
+    fused=None,
+    grad_scale=None,
+    found_inf=None,
+    *,
+    weight_decay,
+    momentum,
+    lr,
+    dampening,
+    nesterov,
+    maximize,
+):
+    for i, (p, grad, momentum_buf) in enumerate(
+        zip(params, d_p_list, momentum_buffer_list)
+    ):
+        g = -grad if maximize else grad
+        if weight_decay != 0.0:
+            g = g + weight_decay * p
+        if momentum != 0.0:
+            if momentum_buf is None:
+                b = g.clone()
+            else:
+                b = momentum_buf * momentum + g * (1 - dampening)
+            momentum_buffer_list[i] = b
+            if nesterov:
+                g = g + momentum * b
+            else:
+                g = b
+        p.sub_(lr * g)
+
+
+class SGD(Optimizer):
+    def __init__(
+        self,
+        params,
+        lr=0.001,
+        momentum=0,
+        dampening=0,
+        weight_decay=0,
+        nesterov=False,
+        *,
+        maximize=False,
+        foreach=None,
+        differentiable=False,
+        fused=None,
+    ):
+        defaults = dict(
+            lr=lr,
+            momentum=momentum,
+            dampening=dampening,
+            weight_decay=weight_decay,
+            nesterov=nesterov,
+            maximize=maximize,
+            foreach=foreach,
+            differentiable=differentiable,
+            fused=fused,
+        )
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            with torch.set_grad_enabled(group["differentiable"]):
+                d_p_list = [p.grad for p in group["params"]]
+                momentum_buffer_list = [
+                    self.state[p]["b"] if "b" in self.state[p] else None
+                    for p in group["params"]
+                ]
+                sgd(
+                    group["params"],
+                    d_p_list,
+                    momentum_buffer_list,
+                    foreach=group["foreach"],
+                    fused=group["fused"],
+                    weight_decay=group["weight_decay"],
+                    momentum=group["momentum"],
+                    lr=group["lr"],
+                    dampening=group["dampening"],
+                    nesterov=group["nesterov"],
+                    maximize=group["maximize"],
+                )
+                for p, buf in zip(group["params"], momentum_buffer_list):
+                    self.state[p]["b"] = buf
+
+        return loss
