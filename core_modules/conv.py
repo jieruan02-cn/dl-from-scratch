fe048544952f54b171f22146ac85b931a205d75c
jieruan <jieruan02@gmail.com>
2026-07-04 20:32:01 -0500

    Refactor Conv to use a shared base and functional.

--- core_modules/conv.py
+++ core_modules/conv.py
@@ -3,175 +3,77 @@ import torch
 import torch.nn as nn
 
 
-def _canonical_padding1d(padding, stride, window_size):
+def _canonical_tuple(input, dim):
+    return (input,) * dim if isinstance(input, int) else input
+
+
+def _canonical_dilated_kernel_size(kernel_size, dilation):
+    out = (torch.tensor(kernel_size) - 1) * torch.tensor(dilation) + 1
+    return tuple(out.tolist())
+
+
+def _canonical_padding(padding, dim, stride, size):
     if isinstance(padding, int):
-        return padding
+        return (padding,) * dim
     elif isinstance(padding, tuple):
-        return padding[0]
+        return padding
     elif isinstance(padding, str):
         if padding == "valid":
-            return 0
+            return (0,) * dim
         elif padding == "same":
-            assert stride == 1 and (window_size - 1) % 2 == 0
-            return (window_size - 1) // 2
+            assert all(x == 1 for x in stride) and all(s % 2 != 0 for s in size)
+            return tuple((s - 1) // 2 for s in size)
         else:
             raise ValueError(f"Expect padding to be valid or same, got {padding}")
     else:
         raise TypeError(f"Expect padding of type int/tuple/str, got {type(padding)}")
 
 
-def _canonical_padding2d(padding, stride_H, stride_W, size_H, size_W):
-    if isinstance(padding, int):
-        return padding, padding
-    elif isinstance(padding, tuple):
-        return padding
-    elif isinstance(padding, str):
-        if padding == "valid":
-            return 0, 0
-        elif padding == "same":
-            assert stride_H == 1 and stride_W == 1
-            assert (size_H - 1) % 2 == 0 and (size_W - 1) % 2 == 0
-            return (size_H - 1) // 2, (size_W - 1) // 2
-        else:
-            raise ValueError(f"Expect padding to be valid or same, got {padding}")
-    else:
-        raise TypeError(f"Expect padding of type int/tuple/str, got {type(padding)}")
+def conv(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
+    B, dim = input.size(0), input.dim() - 2
+    C_out, C_in_per_group, *kernel_size = weight.shape
+    stride = _canonical_tuple(stride, dim)
+    dilation = _canonical_tuple(dilation, dim)
+    size = _canonical_dilated_kernel_size(kernel_size, dilation)
+    padding = _canonical_padding(padding, dim, stride, size)
+    if any(p != 0 for p in padding):
+        pad = tuple(e for pair in reversed(list(zip(padding, padding))) for e in pair)
+        input = nn.functional.pad(input, pad)
+    for i in range(dim):
+        input = input.unfold(dimension=i + 2, size=size[i], step=stride[i])
+    # use slice instead of range
+    indexing_tuple = tuple([slice(None)] * (2 + dim)) + tuple(
+        slice(0, size[i], dilation[i]) for i in range(dim)
+    )
+    input = input[indexing_tuple]
+    out_feature_shape = input.shape[2 : (2 + dim)]
 
-
-def _canonical_tuple2d(input):
-    return (input, input) if isinstance(input, int) else input
-
-
-def _validate_groups(in_channels, groups):
-    if in_channels % groups != 0:
-        raise ValueError(
-            f"Expect in_channels % groups == 0, got in_channels = {in_channels}, groups = {groups}"
-        )
-
-
-def _validate_padding_mode(padding_mode):
-    if padding_mode not in ("zeros", "reflect", "replicate", "circular"):
-        raise ValueError(
-            f"Expect padding_mode to be zeros/reflect/replicate/circular, got {padding_mode}"
-        )
+    input = input.reshape(B, groups, C_in_per_group, *input.shape[2:])
+    input = input.permute(
+        0, 1, *range(3, 3 + dim), 2, *range(3 + dim, 3 + 2 * dim)
+    ).reshape(B, groups, math.prod(out_feature_shape), -1)
+    out = input @ weight.view(groups, C_out // groups, -1).transpose(1, 2)
+    out = out.transpose(-1, -2).reshape(B, C_out, *out_feature_shape)
+    if bias is not None:
+        out = out + bias.view((1, C_out) + (1,) * dim)
+    return out
 
 
 def conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
-    B, C_in, L_in = input.shape
-    C_out, kernel_size = weight.size(0), weight.size(-1)
-    input = input.view(B, groups, C_in // groups, L_in)
-    window_size = (kernel_size - 1) * dilation + 1
-    padding = _canonical_padding1d(padding, stride, window_size)
-    if padding != 0:
-        input = nn.functional.pad(input, (padding, padding), mode="constant", value=0)
-    # need to use tensor.unfold as nn.Unfold or nn.functional.unfold only support 4D.
-    input = input.unfold(-1, window_size, stride).transpose(-2, -3)
-    input = input[:, :, :, :, 0:window_size:dilation].reshape(input.shape[:3] + (-1,))
-
-    out = input @ weight.view(groups, C_out // groups, -1).transpose(1, 2)
-    out = out.transpose(2, 3).reshape(B, C_out, -1)
-    if bias is not None:
-        out = out + bias[None, :, None]
-    return out
-
-
-class Conv1d(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        stride=1,
-        padding=0,
-        dilation=1,
-        groups=1,
-        bias=True,
-        padding_mode="zeros",
-        device=None,
-        dtype=None,
-    ):
-        _validate_groups(in_channels, groups)
-        _validate_padding_mode(padding_mode)
-
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = _canonical_padding1d(
-            self.padding, self.stride, (kernel_size - 1) * dilation + 1
-        )
-        self.dilation = dilation
-        self.groups = groups
-        self.padding_mode = padding_mode
-
-        config = {"device": device, "dtype": dtype}
-        self.weight = nn.Parameter(
-            torch.empty((out_channels, in_channels // groups, kernel_size), **config)
-        )
-        if bias:
-            self.bias = nn.Parameter(torch.empty((out_channels,), **config))
-        else:
-            self.register_parameter("bias", None)
-        self.reset_parameters()
-
-    def forward(self, input):
-        has_batch = input.dim() == 3
-        input = input if has_batch else input.unsqueeze(0)
-        if self.padding != 0:
-            assert self.padding_mode in ("zeros", "reflect", "replicate", "circular")
-            padding_mode = (
-                "constant" if self.padding_mode == "zeros" else self.padding_mode
-            )
-            input = nn.functional.pad(input, (self.padding, self.padding), padding_mode)
-
-        out = conv1d(
-            input,
-            self.weight,
-            bias=self.bias,
-            stride=self.stride,
-            padding=0,
-            dilation=self.dilation,
-            groups=self.groups,
-        )
-        out = out if has_batch else out.squeeze(0)
-        return out
-
-    def reset_parameters(self):
-        bound = math.sqrt(self.groups / (self.in_channels * self.kernel_size))
-        nn.init.uniform_(self.weight, -bound, bound)
-        if self.bias is not None:
-            nn.init.uniform_(self.bias, -bound, bound)
+    return conv(input, weight, bias, stride, padding, dilation, groups)
 
 
 def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
-    B, C_in, H, W = input.shape
-    C_out, C_in_per_group, kernel_H, kernel_W = weight.shape
-    dilation_H, dilation_W = (
-        (dilation, dilation) if isinstance(dilation, int) else dilation
-    )
-    size_H, size_W = (kernel_H - 1) * dilation_H + 1, (kernel_W - 1) * dilation_W + 1
-    stride_H, stride_W = (stride, stride) if isinstance(stride, int) else stride
-    padding_H, padding_W = _canonical_padding2d(
-        padding, stride_H, stride_W, size_H, size_W
-    )
-    if padding_H != 0 or padding_W != 0:
-        input = nn.functional.pad(input, (padding_W, padding_W, padding_H, padding_H))
-
-    input = input.view(B, groups, C_in_per_group, *input.shape[2:])
-    input = input.unfold(3, size_H, stride_H).unfold(4, size_W, stride_W)
-    input = input[:, :, :, :, :, 0:size_H:dilation_H, 0:size_W:dilation_W]
-    L_out_H, L_out_W = input.shape[3:5]
-    input = input.permute(0, 1, 3, 4, 2, 5, 6).reshape(B, groups, L_out_H * L_out_W, -1)
-
-    out = input @ weight.view(groups, C_out // groups, -1).transpose(1, 2)
-    out = out.transpose(-1, -2).reshape(B, C_out, L_out_H, L_out_W)
-    if bias is not None:
-        out = out + bias[None, :, None, None]
-    return out
+    return conv(input, weight, bias, stride, padding, dilation, groups)
 
 
-class Conv2d(nn.Module):
+def conv3d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
+    return conv(input, weight, bias, stride, padding, dilation, groups)
+
+
+class ConvBase(nn.Module):
+    dim = 1
+
     def __init__(
         self,
         in_channels,
@@ -186,20 +88,27 @@ class Conv2d(nn.Module):
         device=None,
         dtype=None,
     ):
-        _validate_groups(in_channels, groups)
-        _validate_padding_mode(padding_mode)
+        if in_channels % groups != 0 or out_channels % groups != 0:
+            raise ValueError(
+                f"Expect in_channels % groups == 0 and out_channels % groups == 0, got in_channels = {in_channels}, out_channels ={out_channels}, groups = {groups}"
+            )
+        if padding_mode not in ("zeros", "reflect", "replicate", "circular"):
+            raise ValueError(
+                f"Expect padding_mode to be zeros/reflect/replicate/circular, got {padding_mode}"
+            )
+        if isinstance(kernel_size, tuple) and self.dim != len(kernel_size):
+            raise ValueError(
+                f"Expect len(kernel_size) == dim, got kernel_size = {kernel_size}, dim = {self.dim}"
+            )
+
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel_size = _canonical_tuple2d(kernel_size)
-        self.stride = _canonical_tuple2d(stride)
-        self.dilation = _canonical_tuple2d(dilation)
-        self.padding = _canonical_padding2d(
-            padding,
-            *self.stride,
-            (self.kernel_size[0] - 1) * self.dilation[0] + 1,
-            (self.kernel_size[1] - 1) * self.dilation[1] + 1,
-        )
+        self.kernel_size = _canonical_tuple(kernel_size, self.dim)
+        self.stride = _canonical_tuple(stride, self.dim)
+        self.dilation = _canonical_tuple(dilation, self.dim)
+        size = _canonical_dilated_kernel_size(self.kernel_size, self.dilation)
+        self.padding = _canonical_padding(padding, self.dim, self.stride, size)
         self.groups = groups
         self.padding_mode = padding_mode
 
@@ -213,14 +122,15 @@ class Conv2d(nn.Module):
         self.reset_parameters()
 
     def forward(self, input):
-        has_batch = input.dim() == 4
+        has_batch = input.dim() == 2 + self.dim
         input = input if has_batch else input.unsqueeze(0)
-        if self.padding != (0, 0):
-            assert self.padding_mode in ("zeros", "reflect", "replicate", "circular")
-            pad = (self.padding[1], self.padding[1], self.padding[0], self.padding[0])
+        if any(p != 0 for p in self.padding):
+            pad = reversed(list(zip(self.padding, self.padding)))
+            pad = tuple(item for pair in pad for item in pair)
             mode = "constant" if self.padding_mode == "zeros" else self.padding_mode
             input = nn.functional.pad(input, pad, mode)
-        out = conv2d(
+
+        out = conv(
             input,
             self.weight,
             bias=self.bias,
@@ -233,9 +143,20 @@ class Conv2d(nn.Module):
         return out
 
     def reset_parameters(self):
-        bound = math.sqrt(
-            self.groups / (self.in_channels * math.prod(self.kernel_size))
-        )
+        kernel_prod = math.prod(self.kernel_size)
+        bound = math.sqrt(self.groups / (self.in_channels * kernel_prod))
         nn.init.uniform_(self.weight, -bound, bound)
         if self.bias is not None:
             nn.init.uniform_(self.bias, -bound, bound)
+
+
+class Conv1d(ConvBase):
+    dim = 1
+
+
+class Conv2d(ConvBase):
+    dim = 2
+
+
+class Conv3d(ConvBase):
+    dim = 3
